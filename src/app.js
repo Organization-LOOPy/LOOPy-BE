a9a67a7ebeb52c316bc547411b083f3d8f3e3c6c
hotchillbread <kimbap0114@gmail.com>
2025-07-11 01:46:26 +0900

    fix:미들웨어 순서 변경

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-import express from 'express';
+import express from "express";
 import { responseHandler } from "./middlewares/responseHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { setupSwagger } from "./config/swagger.js";
-setupSwagger(app);  
 
 const app = express();
 
+setupSwagger(app);
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('루피 백엔드 작동 중!'));
+app.get("/", (req, res) => res.send("루피 백엔드 작동 중!"));
 
 app.use(responseHandler);
+app.use(errorHandler);
 
 export default app;
-
-app.use(errorHandler);
