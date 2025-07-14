9e0a7e8f3ddb8a5650d1ac05b67857668298aa66
hotchillbread <kimbap0114@gmail.com>
2025-07-14 23:32:14 +0900

    feat: 검색 라우터 추가

---
+++ src/routes/searchRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", cafeSearch);
+router.get("/filter", cafeFilter);
+router.get("/map", cafeMap);
+router.post("/map", cafeMapPost);
+
+export default router;
