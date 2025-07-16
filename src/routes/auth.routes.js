2c3914945272c005a4379b64872751404f386583
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-07-16 16:30:28 +0900

    이메일로 로그인된 계정 카카오 연동 기능 구현

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { signup, login } from '../controllers/auth.controller.js';
-import { handleKakaoRedirect } from '../controllers/auth.kakaoController.js';
+import { handleKakaoRedirect, handleKakaoLinkCallback } from '../controllers/auth.kakaoController.js';
+import { authenticateJWT } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.get('/kakao/callback', handleKakaoRedirect);
+router.get('/link/kakao/callback', authenticateJWT, handleKakaoLinkCallback);
+router.get('/kakao/redirect', handleKakaoRedirect);
 
 export default router;
