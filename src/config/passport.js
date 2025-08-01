7a1db984200184012e4b3d12b7ccc6a368fbc70d
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-08-02 02:52:02 +0900

    [docs] swagger 문서에 대시보드 통계 기능 추가

--- src/config/passport.js
+++ src/config/passport.js
@@ -18,7 +18,7 @@ passport.use(
     console.log("🔥 [jwtPayload]:", jwtPayload);
     try {
       const user = await prisma.user.findUnique({
-        where: { id: parseInt(jwtPayload.userId, 10) },
+        where: { id: parseInt(jwtPayload.Id, 10) },
       });
 
       if (user) {
