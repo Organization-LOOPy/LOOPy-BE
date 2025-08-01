e5a03770da133c1c97a5d416f5d0a15c413a02b4
hseo1o2 <0102jhshs@ewha.ac.kr>
2025-08-01 22:11:45 +0900

    fix: stampbook 라우팅 오류 수정

--- src/routes/stampbook.routes.js
+++ src/routes/stampbook.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { issueRewardCoupon } from "../controllers/stampbook.controller.js";
+import {
+    issueRewardCoupon,
+    getMyStampBooks,
+} from "../controllers/stampbook.controller.js";
 import { authenticateJWT } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 router.use(authenticateJWT);
 
+router.get("/stampbooks", getMyStampBooks);
 router.post("/:cafeId/coupon", issueRewardCoupon);
 
 export default router;
