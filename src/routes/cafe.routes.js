c18782272edf54d63702cdc047d6f0b1a2672437
hotchillbread <kimbap0114@gmail.com>
2025-08-03 02:22:19 +0900

    fix: 스웨거 오류

--- src/routes/cafe.routes.js
+++ src/routes/cafe.routes.js
@@ -15,6 +15,8 @@ import { authenticateJWT } from "../middlewares/authMiddleware.js";
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authenticateJWT);
+
 router.use(isCorrectCafeId); // 카페 ID 유효성 검사
 
 router.get("/", getCafe); //카페 정보 조회
