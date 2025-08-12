7ffce9fb35fcb82ec33d608e22cf6d448f3b9038
hotchillbread <kimbap0114@gmail.com>
2025-08-12 14:53:58 +0900

    test..

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -76,8 +76,9 @@ export const updateUserPreferences = async (req, res, next) => {
       req.user.id,
       req.body.preferredKeywords
     );
-    console.log(req.user.id);
+    console.log(result);
     const userId = req.user.id;
+    console.log(userId);
     await userPreferenceEmbedding(
       result.preferredStore,
       result.preferredTakeout,
