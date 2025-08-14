0cc1b5584483bd8e16ee4d35643165406fd2b867
Yujin Gung <yoojin3117@ewhain.net>
2025-08-15 05:30:43 +0900

    Update migration.sql

--- prisma/migrations/20250814183642_update_fk_to_cascade/migration.sql
+++ prisma/migrations/20250814183642_update_fk_to_cascade/migration.sql
@@ -6,8 +6,3 @@ ALTER TABLE `cafes` MODIFY `updated_at` DATETIME(3) NULL;
 
 -- AlterTable
 ALTER TABLE `user_bookmarks` MODIFY `updated_at` DATETIME(3) NULL;
-
--- CreateTable
--- AddForeignKey
--- AddForeignKey
-ALTER TABLE `user_cafe_notifications` ADD CONSTRAINT `user_cafe_notifications_cafe_id_fkey` FOREIGN KEY (`cafe_id`) REFERENCES `cafes`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
