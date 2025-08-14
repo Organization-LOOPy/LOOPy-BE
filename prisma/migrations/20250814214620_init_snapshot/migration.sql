359ede80e772a7c9f7bd59e4aeb7787cfe437ad0
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-08-15 06:47:06 +0900

    마이그레이션 걍 현재 상태 시점으로 스냅샷

---
+++ prisma/migrations/20250814214620_init_snapshot/migration.sql
@@ -0,0 +1,32 @@
+-- DropForeignKey
+ALTER TABLE `challenge_participants` DROP FOREIGN KEY `challenge_participants_joined_cafe_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `notifications` DROP FOREIGN KEY `notifications_cafe_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `point_transactions` DROP FOREIGN KEY `point_transactions_stamp_book_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `stamp_images` DROP FOREIGN KEY `stamp_images_created_by_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `user_role` DROP FOREIGN KEY `user_role_userId_fkey`;
+
+-- DropIndex
+DROP INDEX `stamp_images_created_by_fkey` ON `stamp_images`;
+
+-- AddForeignKey
+ALTER TABLE `user_role` ADD CONSTRAINT `user_role_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `challenge_participants` ADD CONSTRAINT `challenge_participants_joined_cafe_id_fkey` FOREIGN KEY (`joined_cafe_id`) REFERENCES `cafes`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `stamp_books` ADD CONSTRAINT `stamp_books_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `point_transactions` ADD CONSTRAINT `point_transactions_stamp_book_id_fkey` FOREIGN KEY (`stamp_book_id`) REFERENCES `stamp_books`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `notifications` ADD CONSTRAINT `notifications_cafe_id_fkey` FOREIGN KEY (`cafe_id`) REFERENCES `cafes`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
