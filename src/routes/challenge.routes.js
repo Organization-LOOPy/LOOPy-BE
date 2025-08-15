c19292db7f75eb8409a747eb0ffe6e2f562a4101
hseo1o2 <0102jhshs@ewha.ac.kr>
2025-08-15 15:38:50 +0900

    fix: 챌린지참여 로직수정

--- src/routes/challenge.routes.js
+++ src/routes/challenge.routes.js
@@ -17,7 +17,11 @@ router.get("/me", authenticateJWT, getMyChallengeList);
 router.get("/", getChallengeList); // 챌린지 목록 조회
 router.get("/:challengeId/cafes", authenticateJWT, getAvailableStoresForChallenge); // 참여 가능 매장 조회
 router.get("/:challengeId", authenticateJWT, getChallengeDetail); // 챌린지 상세 조회
-router.post("/:challengeId/participate", authenticateJWT, participateInChallenge); // 챌린지 참여
+router.post(
+  "/:cafeId/challenges/:challengeId/join",
+  authenticateJWT,
+  participateInChallenge
+); // 챌린지 참여
 router.post("/:challengeId/complete", authenticateJWT, completeChallenge); // 완료 처리
 
 export default router;
