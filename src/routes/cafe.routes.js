b635c4c1d7b0ebb5fc2570258db58a243ce93ea4
hotchillbread <kimbap0114@gmail.com>
2025-08-18 18:18:00 +0900

    fix: 프론트 수정

--- src/routes/cafe.routes.js
+++ src/routes/cafe.routes.js
@@ -13,23 +13,22 @@ import {
   test,
 } from "../middlewares/cafeMiddleware.js";
 import { authenticateJWT } from "../middlewares/authMiddleware.js";
-
-const router = express();
+const router = express.Router({ mergeParams: true });
 
 router.use(authenticateJWT);
 
 router.use(isCorrectCafeId); // 카페 ID 유효성 검사
 
-router.get("/:cafeId", getCafe); //카페 정보 조회
+router.get("/", getCafe); //카페 정보 조회
 
-router.post("/:cafeId/alram", getNotification);
+router.post("/alram", getNotification);
 
 //router.use(test); // 테스트용 미들웨어, 실제 배포 시 제거
 
-router.get("/:cafeId/review", getCafeReviews); //리뷰 조회
+router.get("/review", getCafeReviews); //리뷰 조회
 
-router.post("/:cafeId/coupon", isMyCoupon, issueCafeCouponToUser); //쿠폰 발급
+router.post("/coupon", isMyCoupon, issueCafeCouponToUser); //쿠폰 발급
 
-router.post("/:cafeId/bookmark", addBookmark); // 북마크 설정
+router.post("/bookmark", addBookmark); // 북마크 설정
 
 export default router;
