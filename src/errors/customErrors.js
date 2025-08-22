632b3554f0c710bb649d018727d119de3883665a
Niicka <lnk1101603@kau.kr>
2025-08-22 21:11:21 +0900

    fix: 메뉴 존재하지 않음 커스텀에러 순서 수정

--- src/errors/customErrors.js
+++ src/errors/customErrors.js
@@ -559,7 +559,7 @@ export class RepresentativeLimitExceededError extends CustomError {
 
 export class CafeMenuNotExistError extends CustomError {
   constructor() {
-    super(404, "CAFE_MENU_NOT_EXIST", "메뉴가 존재하지 않습니다.");
+    super("메뉴가 존재하지 않습니다.", "CAFE_MENU_NOT_EXIST",404);
   }
 }
 
