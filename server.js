5688959d118dcef92306d65d7fa47d87f7920146
yujin5701 <yoojin3117@ewhain.net>
2025-10-08 20:34:54 +0900

    [fix] nginx 도커 내부 네트워크 허용

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
