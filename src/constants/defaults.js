fef11e219254096e43a9e4050de6b3f71b04ecc1
yujin5701 <yoojin3117@ewhain.net>
2025-10-15 19:34:37 +0900

    [fix] user.service 리팩토링

---
+++ src/constants/defaults.js
@@ -0,0 +1,11 @@
+export const DEFAULT_CAFE = {
+  name: '임시 카페 이름',
+  address: '임시 주소',
+  latitude: 0,
+  longitude: 0,
+  ownerName: '임시 사장님 이름',
+  region1DepthName: '임시 시/도',
+  region2DepthName: '임시 시/군/구',
+  region3DepthName: '임시 동/읍/면',
+  businessHours: {},
+};
