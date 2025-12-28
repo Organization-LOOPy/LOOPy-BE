05df39d35ea9db1e72cda3b06450fd26ea825bfc
yujin5701 <yoojin3117@ewhain.net>
2025-12-29 01:38:31 +0900

    [fix] swagger에 이메일 인증 추가, prometheus 메트릭 수집 추가

--- src/routes/metrics.route.js
+++ src/routes/metrics.route.js
@@ -3,6 +3,12 @@ import client from "prom-client";
 
 const router = express.Router();
 
+export const searchKeywordCounter = new client.Counter({
+  name: "search_keyword_total",
+  help: "검색 키워드별 검색 횟수",
+  labelNames: ["keyword"],
+});
+
 router.get("/", async (req, res) => {
   res.set("Content-Type", client.register.contentType);
   res.end(await client.register.metrics());
