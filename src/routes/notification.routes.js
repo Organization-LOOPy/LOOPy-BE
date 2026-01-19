beff9c0daf32eadc0a8c93c80fe434ff7e12019c
hseo1o2 <0102jhshs@ewha.ac.kr>
2026-01-19 22:00:57 +0900

    feat: 알림 로직 추가

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -3,6 +3,7 @@
 import express from "express";
 import {
   getUserNotifications,
+  getUserNotificationsWithCafeInfo,
   getNotificationById,
 } from "../controllers/notification.controller.js";
 import { authenticateJWT } from "../middlewares/authMiddleware.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 router.use(authenticateJWT);
 
 router.get("/users/me/notification", getUserNotifications); // 알림 목록 조회
+router.get("/users/me/notification/with-cafe-info", getUserNotificationsWithCafeInfo); // 알림 목록 조회 (카페 정보 포함)
 
 // 알림 상세 조회 -> 알림 읽음 처리
 router.get("/notification/:notificationId", getNotificationById);
